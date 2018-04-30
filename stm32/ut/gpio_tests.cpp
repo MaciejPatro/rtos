5c0fd1be86b14b3bf577dad89f7a7c3c0c461aaf
Maciej Patro <maciej.patro@gmail.com>
2018-04-30 22:04:25 +0200

    Fix tests naming

--- stm32/ut/gpio_tests.cpp
+++ stm32/ut/gpio_tests.cpp
@@ -10,24 +10,24 @@
 
 namespace stm32 {
 
-TEST_CASE("GPIO basic testing", "[stm32][gpio]")
+TEST_CASE("gpio", "[stm32][gpio]")
 {
   gpio_memory_layout fake_memory{};
   gpio               tested{ fake_memory };
 
-  SECTION("Gpio should correctly setup IO mode as output for given pin")
+  SECTION("should correctly setup IO mode as output for given pin")
   {
     tested.set_mode(gpio::mode::output_pp, io_pin<5>::value);
     REQUIRE(0x00000400U == fake_memory.MODER);
     REQUIRE(0x00000020U == fake_memory.OTYPER);
 
-    SECTION("Additional setup should not reset previous setting")
+    SECTION("additional setup should not reset previous setting")
     {
       tested.set_mode(gpio::mode::output_pp, io_pin<0>::value);
       REQUIRE(0x00000401U == fake_memory.MODER);
       REQUIRE(0x00000021U == fake_memory.OTYPER);
 
-      SECTION("New setup should override previous without changing other pins")
+      SECTION("new setup should override previous without changing other pins")
       {
         tested.set_mode(gpio::mode::input, io_pin<5>::value);
         REQUIRE(0x00000001U == fake_memory.MODER);
@@ -36,27 +36,27 @@ TEST_CASE("GPIO basic testing", "[stm32][gpio]")
     }
   }
 
-  SECTION("Gpio should correctly set pull resistor state")
+  SECTION("should correctly set pull resistor state")
   {
     tested.set_pull_resistor(gpio::pull_resistor::pull_up, io_pin<6>::value);
     tested.set_pull_resistor(gpio::pull_resistor::pull_down, io_pin<5>::value);
     REQUIRE(0x00001800U == fake_memory.PUPDR);
 
-    SECTION("Should change only 5th pin state to clean")
+    SECTION("should change only 5th pin state to clean")
     {
       tested.set_pull_resistor(gpio::pull_resistor::no_pull, io_pin<5>::value);
       REQUIRE(0x00001000U == fake_memory.PUPDR);
     }
   }
 
-  SECTION("Check bit toggling - once it should set the bit")
+  SECTION("should set proper bit via first toggle")
   {
     tested.toggle_pin(io_pin<0>::value);
     tested.toggle_pin(io_pin<1>::value);
     tested.toggle_pin(io_pin<3>::value);
     REQUIRE(0x0000000BU == fake_memory.BSRR);
 
-    SECTION("Second time it should clear the state")
+    SECTION("second time it should clear the state")
     {
       tested.toggle_pin(io_pin<1>::value);
       REQUIRE(0x00000009U == fake_memory.BSRR);
